949b136858a446c94b20ab0f467dcc7d5bc9f57e
Faheem <farooqui.faheem033@gmail.com>
2025-01-23 15:20:20 +0530

    updated

--- python/python_logic_pro.py
+++ python/python_logic_pro.py
@@ -5,14 +5,39 @@ def reverse_string(s:str):
 print(reverse_string("python"))
 
 
-str="I am Faheem"
+str1="I am Faheem"
 
 
-splt=str.split()
+splt=str1.split()
 print(splt)
 
 reverse_string=" ".join(reversed(splt))
 print(reverse_string)
+
+
+print(str1[::-1])
+
+print(str1[:-1])
+
+
+
+##############################################
+def stringreverse(s:str):
+    
+    if not isinstance(s,str):
+        
+        raise ValueError("input must be string")
+    
+    
+    return s[::-1]
+
+try:
+    print(stringreverse("python"))
+    print(stringreverse(54))
+except ValueError as e:
+    print(e)
+    
+################################################
 
 
 
